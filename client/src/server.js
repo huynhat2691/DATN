3e7be79b89671da38def569ebee9efb0dbc0a120
HnB269 <nhathuy2691@gmail.com>
2024-08-31 20:07:01 +0700

    Initial commit

--- client/src/server.js
+++ client/src/server.js
@@ -1,3 +1,5 @@
-export const server = "http://localhost:5000/api/v2";
+export const server =
+  "https://datnserver-7d7s4n98q-huynhat2691s-projects.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:5000/";
+export const backend_url =
+  "https://datnserver-7d7s4n98q-huynhat2691s-projects.vercel.app/";
